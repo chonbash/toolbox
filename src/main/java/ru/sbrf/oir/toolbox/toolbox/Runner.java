9344a26f7e6022354705fc39a04d33ff329d7c2c
chonbash <zhdanovd@10.22.84.201>
2015-08-17 16:38:50 +0400

    Update JVM to 1.6

--- src/main/java/ru/sbrf/oir/toolbox/toolbox/Runner.java
+++ src/main/java/ru/sbrf/oir/toolbox/toolbox/Runner.java
@@ -1,5 +1,14 @@
 package ru.sbrf.oir.toolbox.toolbox;
 
 public class Runner {
+	
+	public static void main(String[] args) throws Exception {
+		
+		Property prop = new Property();
+		PropertyHelper.write(prop, "Property.xml");
+		Property propTest = PropertyHelper.read("Property.xml");
+		
+
+	}
 
 }
